5fd15c5311f7160d44a89b54b4f3849a143f9bda
HybridUofA <benjaminlukeedger@gmail.com>
2025-02-25 19:40:55 +0000

    Public copier from static

--- src/helper_functions.py
+++ src/helper_functions.py
@@ -1,6 +1,7 @@
 from text_to_textnode import text_to_textnodes
 from textnode import TextType, TextNode
 from htmlnode import text_node_to_html_node
+import os, shutil
 
 def text_to_children(text):
     text_nodes = text_to_textnodes(text)
@@ -15,3 +16,20 @@ def count_header_level(block):
             break
 
     return level
+
+def copy_static(source_dir, dest_dir):
+    if os.path.exists(dest_dir):
+        shutil.rmtree(dest_dir)
+    os.mkdir(dest_dir)
+
+    for item in os.listdir(source_dir):
+        item_path = os.path.join(source_dir, item)
+        if os.path.isfile(item_path):
+            print(f"Copying file: {item_path}")
+            shutil.copy(item_path, dest_dir)
+        else:
+            dir_path = os.path.join(source_dir, item)
+            copy_path = os.path.join(dest_dir, item)
+            print(f"Creating directory: {copy_path}")
+            os.mkdir(copy_path)
+            copy_static(dir_path, copy_path)
--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 from helper_functions import copy_static
 
 def main():
-    
+    copy_static("/home/ec2-user/sitegen/static", "/home/ec2-user/sitegen/public")
+
+
 main()
